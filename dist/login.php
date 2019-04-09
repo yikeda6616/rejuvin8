f8a49a414d3c6ce1ab88d5625d35a1c958788a44
Yasushi Ikeda <yikeda6616@gmail.com>
2019-04-08 22:41:27 -0700

    add login and hash

---
+++ dist/login.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+if ($_SESSION['login']) {
+    header('location: message.php');
+}
+
+require './database.php';
+
+$title = 'Rejuvin8';
+
+require './partials/_header.php';
+
+?>
+
+  <div class="container signin-container">
+    
+    <form class="form-signin" action="verify.php" method=post>
+      <div class="text-center mb-4">
+        <img class="loginForm__logo mb-4" src="./img/logo.png" alt="brand logo">
+        <h1 class="h3 mb-3 font-weight-normal">Please Sign in</h1>
+      </div>
+
+      <div class="form-label-group">
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="email" required autofocus>
+        <label for="inputEmail">Email address</label>
+      </div>
+
+      <div class="form-label-group">
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
+        <label for="inputPassword">Password</label>
+      </div>
+
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted text-center">&copy; Rejuvin8 <?= date('Y'); ?></p>
+    </form>
+  </div>
+
+<?php
+
+require './partials/_footer.php';
---
+++ dist/verify.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+include './database.php';
+
+$post_email = $_POST['email'];
+
+$sql = 'SELECT * FROM user WHERE email = :email';
+$stmt = $pdo->prepare($sql);
+$stmt->execute([':email' => $post_email]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$user) {
+    echo'erorr';
+    // header('location: login.php?error=true');
+}
+
+if (password_verify($_POST['password'], $user['hash'])) {
+    $_SESSION['login'] = true;
+    header('location: ./message.php?success=true');
+} else {
+    header('location: ./login.php?error=true');
+}
