c6ec9bd2aedcb93b86f903f9cfcc0b0f01874842
Yasushi Ikeda <yikeda6616@gmail.com>
2019-04-10 23:36:53 -0700

    get message list

--- dist/admin/message.php
+++ dist/admin/message.php
@@ -9,10 +9,7 @@ if (isset($_SESSION['user_id'])) {
 
 $pdo = DB::connect();
 $sql = 'SELECT * FROM subscribe';
-$stmt = $pdo->prepare($sql); // Use prepared statement to prevent SQL injection
-$stmt->execute();
-$subscribe = $stmt->fetch(PDO::FETCH_ASSOC);
-var_dump($subscribe);
+$stmt = $pdo->query($sql);
 
 include './partials/_header.php';
 
@@ -22,8 +19,7 @@ include './partials/_header.php';
 
   <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <h2>Pages</h2>
-      <a href="pageForm.php"><i class="fas fa-plus-circle"></i> Add Page</a>
+      <h2>Subscribes</h2>
     </div>
 
     <h3>Manage Your Pages</h3>
@@ -37,10 +33,10 @@ include './partials/_header.php';
           </tr>
         </thead>
         <tbody>
-          <?php while ($row = mysqli_fetch_assoc($page)): ?>
+          <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
           <tr>
-            <td><a href="../index.php?id=<?= $row['id']; ?>" target="_brank"><?= $row['name']; ?></a></td>
-            <td class="text-center"><a href="pageForm.php?page_id=<?= $row['id']; ?>"><i class="fas fa-envelope"?></i></a></td>
+            <td><?= $row['name']; ?></td>
+            <td class="text-center"><a href="pageForm.php?page_id=<?= $row['id']; ?>"><?= $row['email']; ?><i class="fas fa-envelope"?></i></a></td>
             <td class="text-center"><a href="actions/deletePage.php?page_id=<?= $row['id']; ?>"><i class="fas fa-trash-alt"?></i></a></td>
           </tr>
           <?php endwhile; ?>
