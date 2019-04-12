618a246bf06a5ce090a051a35f423df8f85288f7
Yasushi Ikeda <yikeda6616@gmail.com>
2019-04-11 23:29:12 -0700

    change from h() to filter_input

--- dist/admin/login.php
+++ dist/admin/login.php
@@ -5,10 +5,13 @@ session_start();
 include './function.php';
 include './DB.php';
 
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
 $pdo = DB::connect();
 $sql = 'SELECT * FROM user WHERE email = ?';
-$stmt = $pdo->prepare($sql); // Use prepared statement to prevent SQL injection
-$stmt->bindValue(1, h($_POST['email']));
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(1, $email);
 $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -16,7 +19,7 @@ if (!$user) {
     header('location: login.php?error=true');
 }
 
-if (password_verify(h($_POST['password']), $user['hash'])) {
+if (password_verify($password, $user['hash'])) {
     $_SESSION['user_id'] = $user['id'];
     header('location: ./message.php?success=true');
 } else {
