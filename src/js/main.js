16316123be2d3880f40c3d062e9f22fa8dbbb829
Yasushi Ikeda <yikeda6616@gmail.com>
2019-04-12 21:17:39 -0700

    add fixed header

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,10 @@ window.addEventListener('scroll', () => {
   const offset = window.pageYOffset;
 
   if (offset < 70) {
-    console.log('detach');
     header.classList.remove('fixed');
     headerContent.style.cssText =
       'border-bottom: 1px solid rgba(255,255,255,0.15); padding-bottom: 40px;';
   } else {
-    console.log('fix');
     header.classList.add('fixed');
     headerContent.style.cssText = 'border-bottom: none; padding-bottom: 0;';
   }
